185686fee5696fc02f5ce00694330c42f1d8f7c4
utilisateur <bertrand.guiral@gmail.com>
2018-01-22 18:19:07 +0100

    bg

--- Midas/src/main/java/bg/panama/btc/swing/PanelOrderBook.java
+++ Midas/src/main/java/bg/panama/btc/swing/PanelOrderBook.java
@@ -2,8 +2,12 @@ package bg.panama.btc.swing;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -14,14 +18,24 @@ import bg.panama.btc.model.OrderBookItem;
 public class PanelOrderBook extends JPanel{
 
 	private static final long serialVersionUID = 1L;
+	private static final DateFormat df = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss");
 	int w = 500;
 	int h = 500;
 	Dimension dim = new Dimension(w, h);
 	OrderBook book ;
 	List<Item> list = new ArrayList<>();
-	
-	public PanelOrderBook(OrderBook book, List<OrderBookItem> l) {
+	Date date = new Date();
+	String dateStr ="";
+	Font myFont = new Font ("Courier New", Font.BOLD, 25);
+	double ammountTotalAsk=0;
+	double ammountTotalBids =0;
+	int ammountTotalAskPercent = 0;
+	int ammountToyalBidPercent =0;
+	String label="";
+	public PanelOrderBook(Date date, OrderBook book, List<OrderBookItem> l) {
 		this.book=book;
+		this.date = date;
+		dateStr=df.format(date);
 		this.setPreferredSize(dim);
 		this.setSize(dim);
 		l.addAll(book.getListAsks());
@@ -41,20 +55,29 @@ public class PanelOrderBook extends JPanel{
 		}
 		double deltaPrice  = maxPrice - minPrice;
 		list = new ArrayList<>();
+		ammountTotalAsk=0;
+		ammountTotalBids=0;
 		for(OrderBookItem obi2 : book.getListAsks()){
-			int ww =(int) ((obi2.getPrice()-minPrice)*w/deltaPrice);
+			int www =(int) ((obi2.getPrice()-minPrice)*w/deltaPrice);
 			int hhh =  (int)(((obi2.getAmount())/amountMAx) * h);
 			int hh = h - hhh;
+			int ww = w - www;
 			Item item = new Item(Color.green,ww,hh);
 			list.add(item);
+			ammountTotalAsk+= obi2.getAmount();
 		}
 		for(OrderBookItem obi2 : book.getListBids()){
-			int ww =(int) ((obi2.getPrice()-minPrice)*w/deltaPrice);
+			int www =(int) ((obi2.getPrice()-minPrice)*w/deltaPrice);
 			int hhh =  (int)(((obi2.getAmount())/amountMAx) * h);
 			int hh = h - hhh;
+			int ww = w - www;
 			Item item = new Item(Color.red,ww,hh);
 			list.add(item);
+			ammountTotalBids+= obi2.getAmount();
 		}
+		ammountTotalAskPercent= (int) (ammountTotalAsk*100/(ammountTotalAsk+ammountTotalBids));
+		ammountToyalBidPercent= (int) (ammountTotalBids*100/(ammountTotalAsk+ammountTotalBids));
+		label  ="Ask : "+ammountTotalAskPercent+" % Bids "+ammountToyalBidPercent+" %";
 	}
 	@Override
 	public void paint(Graphics g) {
@@ -64,7 +87,11 @@ public class PanelOrderBook extends JPanel{
 			g.setColor(item.color);
 			g.fillRect(item.w, item.h, 5, h-item.h);
 		}
+		g.setColor(Color.BLACK);
+		g.setFont(myFont);
+		g.drawString(dateStr, 20, 20);
 		
+		g.drawString(label, 20, 60);
 	}
 
 	static class Item {
--- Midas/src/main/java/bg/panama/btc/swing/PanelOrderBookHistory.java
+++ Midas/src/main/java/bg/panama/btc/swing/PanelOrderBookHistory.java
@@ -1,6 +1,7 @@
 package bg.panama.btc.swing;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JTabbedPane;
@@ -20,13 +21,14 @@ public class PanelOrderBookHistory {
     List<PanelOrderBook> listPAnelOrderBook = new ArrayList<>();
     
 	public void addOrderBook(OrderBook book) {
-		PanelOrderBook panelOrderBook = new PanelOrderBook(book, listTotaleBookOrderItem);
+		PanelOrderBook panelOrderBook = new PanelOrderBook(new Date(),book, listTotaleBookOrderItem);
 		
 		this.tabbedPane.addTab(""+i++,panelOrderBook);
 		panelOrderBook.setVisible(true);
 		for(PanelOrderBook p : listPAnelOrderBook){
 			p.update(listTotaleBookOrderItem);
 		}
+		tabbedPane.setSelectedIndex(i-1);
 	}
 	
 	
